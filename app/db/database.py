d018b2531f1c8ae044d93707a288609d4ea7de08
furqanrupom <furqanrupom978@gmail.com>
2026-08-03 00:22:47 +0600

    chore(db): update formation

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 from app.core.config import settings
-
-
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 Base = declarative_base()  # create Base here
+
+
 def get_db():
     """Dependency to get database session"""
     db = SessionLocal()
@@ -25,5 +26,3 @@ def create_tables():
 def drop_tables():
     """Drop all tables in the database"""
     Base.metadata.drop_all(bind=engine)
-
-
