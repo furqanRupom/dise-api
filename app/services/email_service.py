e6893e9779e7f029a80c940872384b145490e5db
furqanrupom <furqanrupom978@gmail.com>
2026-08-02 22:12:06 +0600

    feat(auth): added forgot password mail service, jinja templates

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -15,3 +15,18 @@ class EmailService:
             subtype=MessageType.html,
         )
         background_tasks.add_task(fm.send_message, message, template_name="otp.html")
+
+    @staticmethod
+    async def send_forgot_password_mail(
+        email: str, otp: str, background_tasks: BackgroundTasks
+    ):
+        message = MessageSchema(
+            subject="Reset your password",
+            recipients=[NameEmail(name=email, email=email)],
+            template_body={"otp": otp},
+            subtype=MessageType.html,
+        )
+
+        background_tasks.add_task(
+            fm.send_message, message, template_name="forgot-password.html"
+        )
