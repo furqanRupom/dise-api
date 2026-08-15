9ffc9ced592d92d4c78388a54ae67ac7b88682a2
furqanrupom <furqanrupom978@gmail.com>
2026-08-15 23:41:25 +0600

    feat(user): add avatar resizing and compression with Pillow

--- app/core/cloudinary.py
+++ app/core/cloudinary.py
@@ -1,5 +1,9 @@
+from io import BytesIO
+
 import cloudinary
 import cloudinary.uploader
+from fastapi import HTTPException, UploadFile, status
+from PIL import Image, UnidentifiedImageError
 
 from app.core.config import settings
 
@@ -10,10 +14,40 @@ cloudinary.config(
     secure=True,
 )
 
+ALLOWED_TYPES = ["image/jpeg", "image/png", "image/webp"]
 
-def upload_image(file):
+MAX_FILE_SIZE = 5 * 1024 * 1024
+
+
+def upload_image(file: UploadFile) -> str:
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Only JPEG, PNG, and WebP images are allowed",
+        )
+    contents = file.file.read()
+
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Maximum file size is 5MB.",
+        )
+
+    try:
+        image = Image.open(BytesIO(contents))
+    except UnidentifiedImageError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid image file",
+        )
+
+    image = image.convert("RGB")
+    image.thumbnail((500, 500))
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG", qualify=85, optimize=True)
+    buffer.seek(0)
     result = cloudinary.uploader.upload(
-        file.file,
+        buffer,
         folder=settings.CLOUDINARY_FOLDER,
     )
     return result["url"]
