c465eefc10f3f67e5754116123ae6edb913eee50
furqanrupom <furqanrupom978@gmail.com>
2026-08-15 23:17:27 +0600

    feat(cloudinary): added cloudinary config for file uploading

---
+++ app/core/cloudinary.py
@@ -0,0 +1,23 @@
+import cloudinary
+import cloudinary.uploader
+
+from app.core.config import settings
+
+cloudinary.config(
+    cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+    api_key=settings.CLOUDINARY_API_KEY,
+    api_secret=settings.CLOUDINARY_API_SECRET,
+    secure=True,
+)
+
+
+def upload_image(file):
+    result = cloudinary.uploader.upload(
+        file.file,
+        folder=settings.CLOUDINARY_FOLDER,
+    )
+    return result["url"]
+
+
+def delete_image(url):
+    cloudinary.uploader.destroy(url)
