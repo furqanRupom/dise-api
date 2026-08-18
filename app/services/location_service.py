c815e24b66cb7f9706547d84f48f3b87e128e282
furqanrupom <furqanrupom978@gmail.com>
2026-08-18 16:17:22 +0600

    feat(location): added update and create services in location

--- app/services/location_service.py
+++ app/services/location_service.py
@@ -1,14 +1,46 @@
 import uuid
 
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.models import Location
+from app.schemas.location import LocationCreate, LocationUpdate
 from app.schemas.response import SendRespose
 
 
 class LocationService:
     def __init__(self, db: Session):
         self.db = db
+
+    def create_location(
+        self,
+        payload: LocationCreate,
+    ):
+        location = Location(**payload.model_dump())
+        self.db.add(location)
+        self.db.commit()
+        return SendRespose(
+            success=True,
+            message="Location created successfully",
+            data=location,
+        )
+
+    def update_location(
+        self,
+        location_id: uuid.UUID,
+        payload: LocationUpdate,
+    ):
+        location = self.db.query(Location).filter(Location.id == location_id).first()
+        if location:
+            for key, value in payload.model_dump().items():
+                setattr(location, key, value)
+            self.db.commit()
+            return SendRespose(
+                success=True,
+                message="Location updated successfully",
+                data=location,
+            )
+        raise HTTPException(status_code=404, detail="Location not found")
 
     def get_location(
         self,
