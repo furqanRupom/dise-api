b021106745a7004df10456880339fe24a53e99fc
furqanrupom <furqanrupom978@gmail.com>
2026-08-18 16:19:24 +0600

    feat(location): implemented all of Rest APIs for location modules

--- app/api/location.py
+++ app/api/location.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from app.core.dependencies import require_admin
 from app.db import get_db
 from app.models import User
+from app.schemas.location import LocationCreate, LocationResponse, LocationUpdate
 from app.services.location_service import LocationService
 
 router = APIRouter(
@@ -15,7 +16,28 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.post("/", response_model=LocationResponse)
+async def create_location(
+    payload: LocationCreate,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(require_admin)],
+):
+    location = LocationService(db)
+    return location.create_location(payload)
+
+
+@router.put("/{location_id}", response_model=LocationResponse)
+async def update_location(
+    location_id: uuid.UUID,
+    payload: LocationUpdate,
+    db: Annotated[Session, Depends(get_db)],
+    current_user: Annotated[User, Depends(require_admin)],
+):
+    location = LocationService(db)
+    return location.update_location(location_id, payload)
+
+
+@router.get("/", response_model=list[LocationResponse])
 async def get_location(
     db: Annotated[Session, Depends(get_db)],
     current_user: Annotated[User, Depends(require_admin)],
@@ -24,7 +46,7 @@ async def get_location(
     return location.get_location()
 
 
-@router.get("/{location_id}")
+@router.get("/{location_id}", response_model=LocationResponse)
 async def get_location_by_id(
     location_id: uuid.UUID,
     db: Annotated[Session, Depends(get_db)],
@@ -34,7 +56,7 @@ async def get_location_by_id(
     return location.get_location_by_id(location_id)
 
 
-@router.delete("/{location_id}")
+@router.delete("/{location_id}", response_model=LocationResponse)
 async def delete_location(
     location_id: uuid.UUID,
     db: Annotated[Session, Depends(get_db)],
